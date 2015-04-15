3271d081f2bcfdd07972d68d5d5699f1c97dd38b
Christof Rabensteiner <christof.rabensteiner@student.tugraz.at>
2015-04-15 10:36:51 +0200

    init REST API
    - hello world example
    - check if it works via: 
    - http://[host]:1990/confluence/rest/timesheet/1.0/timesheets should return "hello world"

---
+++ src/main/java/org/catrobat/confluence/rest/RestHelloWorldService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.catrobat.confluence.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@Path("/")
+@Produces({MediaType.APPLICATION_JSON})
+public class RestHelloWorldService {
+    @GET
+    @Path("timesheets")
+    public Response getUncompletedUsers() {
+        return Response.ok("hello world").build();
+    }
+}

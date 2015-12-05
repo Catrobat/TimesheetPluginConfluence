fe7c590e8adb0935f4dccb4e95e0782c9825952a
Adrian Schnedlitz <a.schnedlitz@student.tugraz.at>
2015-12-05 14:26:07 +0100

    added visualization page + cleaned up code and table; Sum of timesheet entries is calculated and displayed

--- src/main/resources/js/visualization.js
+++ src/main/resources/js/visualization.js
@@ -36,7 +36,7 @@ function fetchData() {
 
 	AJS.$.when(timesheetFetched, categoriesFetched, teamsFetched, entriesFetched)
 		.done(assembleTimesheetData)
-		.done(populateTable, prepareImportDialog)
+		.done(populateTable)
 		.fail(function (error) {
 			AJS.messages.error({
 				title: 'There was an error while fetching data.',
@@ -71,490 +71,52 @@ function assembleTimesheetData(timesheetReply, categoriesReply, teamsReply, entr
 function populateTable(timesheetDataReply) {
 
 	var timesheetData = timesheetDataReply[0];
-	var timesheetTable = AJS.$("#timesheet-table");
+
+	var timesheetTable = AJS.$("#visualization-table");
 	timesheetTable.empty();
 
-	timesheetTable.append(Confluence.Templates.Timesheet.timesheetHeader(
+	timesheetTable.append(Confluence.Templates.Visualization.visualizationHeader(
 					{teams: timesheetData.teams}
 	));
-	
-	var emptyEntry = {
-		entryID: "new-id",
-		date: "",
-		begin: "",
-		end: "",
-		pause: "00:00",
-		description: "",
-		duration: ""
-	};
-
-	var addNewEntryOptions = {
-		httpMethod : "post",
-		callback   : addNewEntryCallback,
-		ajaxUrl    : restBaseUrl + "timesheets/" + timesheetData.timesheetID + "/entry/"
-	};
-	
-	var emptyForm = renderFormRow(timesheetData, emptyEntry, addNewEntryOptions);
-	timesheetTable.append(emptyForm);
 
 	appendEntriesToTable(timesheetData);
 }
 
 function appendEntriesToTable(timesheetData) {
 	
-	var timesheetTable = AJS.$("#timesheet-table");
-	
-	timesheetData.entries.map(function (entry) {
-		var viewRow = renderViewRow(timesheetData, entry);
-		timesheetTable.append(viewRow);
-	});
-}
+	var timesheetTable = AJS.$("#visualization-table");
 
-function prepareImportDialog(timesheetDataReply) {
-	
-	var timesheetData = timesheetDataReply[0];
-	
-	var showImportDialogButton = AJS.$(".import-google-docs");
-	var importDialog = AJS.$(".import-dialog"); 
-	var importTextarea = importDialog.find(".import-text");
-	var startImportButton = importDialog.find(".start-import");
-	
-	showImportDialogButton.click(function() {
-		AJS.dialog2(importDialog).show();
-	});
-	
-	autosize(importTextarea);
-	
-	startImportButton.click(function() {
-		importGoogleDocsTable(importTextarea.val(), timesheetData, importDialog);
-	});
-}
+  var month = "";
+  var sum = 0;
 
-function importGoogleDocsTable(table, timesheetData, importDialog) {
-	var entries = parseEntriesFromGoogleDocTimesheet(table, timesheetData);
-	var url = restBaseUrl + "timesheets/" + timesheetID + "/entries";
-	
-	if(entries.length === 0) return;
-	
-	AJS.$.ajax({
-		type: "post",
-		url : url,
-		contentType: "application/json",
-		data: JSON.stringify(entries)
-	})
-	.then(function(response) {
-		showImportMessage(response);
-		AJS.dialog2(importDialog).hide();
-		timesheetData.entries = response.entries;
-		appendEntriesToTable(timesheetData);
-	})
-	.fail(function (error) {
-		AJS.messages.error({
-			title: 'There was an error during your Google Timesheet import.',
-			body: '<p>Reason: ' + error.responseText + '</p>'
-		});
-	});
-}
+  for(var key in timesheetData.entries) {
+    //noch einbauen ob im selben Montag
 
-function showImportMessage(response) {
-	var successfulEntries = response.entries.length;
-	var errorEntries = response.errorMessages.length ;
-	
-	if(errorEntries > 0) {
-		
-		var begin = (successfulEntries === 0)
-						? "Entries could not be imported"
-						: "Some entries could not be imported";
-		
-		var message = begin + ". Reason: <br /> " 
-						+ "<ul><li>"
-						+ response.errorMessages.join("</li><li>")
-						+ "</li></ul>"
-						+ "Successfully imported entries: " + successfulEntries + "<br />"
-						+ "Failed imports: " + errorEntries + "<br />";
-				
-		if (successfulEntries === 0) 
-			AJS.messages.error({title: 'Import Error',body:  message});
-		else 
-			AJS.messages.warning({title: 'Import Error',body:  message});
-		
-	} else {
-		var message = "Imported " + successfulEntries + " entries.";
-		AJS.messages.success({
-			title: 'Import was successful!',
-			body:  message
-		});
-	}
-}
+    var hours = calculateDuration(timesheetData.entries[key].beginDate, timesheetData.entries[key].endDate,
+    timesheetData.entries[key].pauseMinutes).getHours();
+    var minutes = calculateDuration(timesheetData.entries[key].beginDate, timesheetData.entries[key].endDate,
+    timesheetData.entries[key].pauseMinutes).getMinutes();
 
-/**
- * Callback after creating new Entry
- * @param {Object} entry
- * @param {Object} timesheetData
- * @param {jQuery} form
- */
-function addNewEntryCallback(entry, timesheetData, form) {
-	var viewRow = renderViewRow(timesheetData, entry);
-	var beginTime = form.beginTimeField.timepicker('getTime');
-	var endTime = form.endTimeField.timepicker('getTime');
-
-	form.row.after(viewRow);
-	form.beginTimeField.timepicker('setTime', endTime);
-	form.endTimeField.timepicker('setTime', new Date(2 * endTime - beginTime));
-	form.pauseTimeField.val("00:00").trigger('change');
-}
-
-/**
- * Callback after editing an entry
- * @param {Object} entry
- * @param {Object} timesheetData
- * @param {jQuery} form
- */
-function editEntryCallback(entry, timesheetData, form) {
-	var newViewRow = prepareViewRow(timesheetData, entry); //todo check if entry is augmented
-	var oldViewRow = form.row.prev();
-	
-	newViewRow.find("button.edit").click(function () {
-	newViewRow.hide();
-	form.row.show();
-	});
-
-	newViewRow.find("button.delete").click(function () {
-		deleteEntryClicked(newViewRow, entry.entryID);
-	});
-	
-	oldViewRow.after(newViewRow);
-	oldViewRow.remove();
-	
-	form.row.hide(); 
-}
-
-/**
- * Handles saving an entry
- * @param {Object} timesheetData
- * @param {Object} saveOptions
- *           callback   : Function(entry, timesheetData, form)
- *           ajaxUrl    : String
- *           httpMethod : String
- * @param {jQuery} form
- * @returns {undefined}
- */
-function saveEntryClicked(timesheetData, saveOptions, form) {
-	form.saveButton.prop('disabled', true);
-
-	var date = form.dateField.val();
-  var validDateFormat = new Date(date);
-
-	if((date == "") || (isValidDate(validDateFormat) === false)) {
-	  date = new Date().toJSON().slice(0,10);
-	}
-
-	var beginTime = form.beginTimeField.timepicker('getTime');
-
-	if(beginTime == null) {
-	  beginTime = new Date;
-	}
-
-	var endTime   = form.endTimeField.timepicker('getTime');
-
-	if(endTime == null) {
-	  endTime = new Date;
+    sum = sum + hours + minutes / 60;
   }
+  //console.log(sum);
+  var emptyEntry = {
+    entryID: "new-id",
+    date: "",
+    begin: "",
+    end: "",
+    pause: "00:00",
+    description: "",
+    duration: sum
+  };
 
-	var pauseTime = form.pauseTimeField.timepicker('getTime');
-	var beginDate = new Date(date + " " + toTimeString(beginTime));
-	var endDate   = new Date(date + " " + toTimeString(endTime));
-	var pauseMin  = pauseTime.getHours() * 60 + pauseTime.getMinutes();
+  var viewRow = AJS.$(Confluence.Templates.Visualization.visualizationEntry(
+  					{entry: emptyEntry, teams: timesheetData.teams}));
+  timesheetTable.append(viewRow);
 
-	var entry = {
-		beginDate: beginDate,
-		endDate: endDate,
-		description: form.descriptionField.val(),
-		pauseMinutes: pauseMin,
-		teamID: form.teamSelect.val(),
-		categoryID: form.categorySelect.val()
-	};
-
-	form.loadingSpinner.show();
-
-	AJS.$.ajax({
-		type: saveOptions.httpMethod,
-		url:  saveOptions.ajaxUrl,
-		contentType: "application/json",
-		data: JSON.stringify(entry) //causes error in FIREFOX
-	})
-	.then(function(entry) {
-		var augmentedEntry = augmentEntry(timesheetData, entry);
-		saveOptions.callback(augmentedEntry, timesheetData, form);
-	})
-	.fail(function (error) {
-	  console.log(error);
-		AJS.messages.error({
-			title: 'There was an error while saving.',
-			body: '<p>Reason: ' + error.responseText + '</p>'
-		});
-	})
-	.always(function () {
-		form.loadingSpinner.hide();
-		form.saveButton.prop('disabled', false);
-	});
-}
-
-/**
- * creates a form with working ui components and instrumented buttons
- * @param {Object} timesheetData
- * @param {Object} entry
- * @param {Object} saveOptions
- *           callback   : Function(entry, timesheetData, form)
- *           ajaxUrl    : String
- *           httpMethod : String
- * @returns {jquery} form
- */
-function renderFormRow(timesheetData, entry, saveOptions) {
-
-	if (entry.pause === "") {
-		entry.pause = "00:00";
-	}
-
-	var form = prepareForm(entry, timesheetData);
-
-	form.saveButton.click(function () {
-		saveEntryClicked(timesheetData, saveOptions, form);
-	});
-
-	return form.row;
-}
-
-/**
- * Create form for editing a entry & instrument ui components
- * @param {object} entry
- * @param {object} timesheetData
- * @returns {object of jquery objects} 
- */
-function prepareForm(entry, timesheetData) {
-
-	var teams = timesheetData.teams;
-	var row = $(Confluence.Templates.Timesheet.timesheetEntryForm(
-					{entry: entry, teams: teams})
-	);
-
-	var form = {
-		row: row,
-		loadingSpinner:   row.find('span.aui-icon-wait').hide(),
-		saveButton:       row.find('button.save'),
-		dateField:        row.find('input.date'),
-		beginTimeField:   row.find('input.time.start'),
-		endTimeField:     row.find('input.time.end'),
-		pauseTimeField:   row.find('input.time.pause'),
-		durationField:    row.find('input.duration'),
-		descriptionField: row.find('input.description'),
-		categorySelect:   row.find('span.category'),
-		teamSelect:       row.find('select.team')
-	};
-
-	//date time columns
-	form.dateField
-		.datePicker(
-			{overrideBrowserDefault: true, languageCode: 'de'}
-		);
-
-	row.find('input.time.start, input.time.end')
-		.timepicker({
-			showDuration: false,
-			timeFormat: 'H:i',
-			scrollDefault: 'now',
-			step: 15
-		});
-
-	form.pauseTimeField.timepicker({timeFormat: 'H:i', step: 15})
-		.change(changePauseTimeField)
-		.on('timeFormatError', function () {
-			this.value = '00:00';
-		});
-
-	new Datepair(row.find(".time-picker")[0]);
-
-	row.find('input.time')
-		.change(function () {
-			updateTimeField(form);
-		});
-
-	var initTeamID = (entry.teamID !== undefined)
-				? entry.teamID : Object.keys(teams)[0];
-
-	form.teamSelect.auiSelect2()
-		.change(function() {
-			var selectedTeamID = this.value;
-			updateCategorySelect(form.categorySelect, selectedTeamID, entry, timesheetData);
-		})
-		.auiSelect2("val", initTeamID)
-		.trigger("change");
-
-	if (countDefinedElementsInArray(teams) < 2) {
-		row.find(".team").hide();
-	}
-	
-	return form;
-}
-
-function parseEntriesFromGoogleDocTimesheet(googleDocContent, timesheetData) {
-	var entries = [];
-	
-	googleDocContent
-		.split("\n")
-		.forEach(function(row){
-			if(row.trim() === "") return;
-			var entry = parseEntryFromGoogleDocRow(row, timesheetData); 
-			entries.push(entry);
-		});
-	
-	return entries;
-}
-
-function parseEntryFromGoogleDocRow(row, timesheetData) {
-	var pieces = row.split("\t");
-	
-	var firstTeamID = Object.keys(timesheetData.teams)[0];
-	var firstTeam   = timesheetData.teams[firstTeamID];
-	var firstCategoryIDOfFirstTeam = firstTeam.teamCategories[0];
-	
-	return {
-		description  : pieces[6],
-		pauseMinutes : getMinutesFromTimeString(pieces[4]),
-		beginDate    : new Date(pieces[0] + " " + pieces[1]),
-		endDate      : new Date(pieces[0] + " " + pieces[2]),
-		teamID			 : firstTeamID,
-		categoryID	 : firstCategoryIDOfFirstTeam
-	};
-} 
-
-/**
- * Updates the Category Seletion Box depending on the selected team
- * @param {jQuery} categorySelect
- * @param {int} selectedTeamID
- * @param {Object} entry
- * @param {Object} timesheetData
- */
-function updateCategorySelect(categorySelect, selectedTeamID, entry, timesheetData) {
-
-	var selectedTeam = timesheetData.teams[selectedTeamID];
-	var categoryPerTeam = filterCategoriesPerTeam(selectedTeam, timesheetData.categories);
-
-	categorySelect.auiSelect2({data : categoryPerTeam});
-
-	var selectedCategoryID = (entry.categoryID === undefined || selectedTeamID != entry.teamID)
-		? selectedTeam.teamCategories[0]
-		: entry.categoryID;
-
-	categorySelect.val(selectedCategoryID).trigger("change");
-}
-
-/**
- * Creates an array with the categories of seletedTeam
- * @param {Object} selectedTeam
- * @param {Object} categories
- * @returns {Array of Objects}
- */
-function filterCategoriesPerTeam(selectedTeam, categories) {
-
-	var categoriesPerTeam = [];
-
-	selectedTeam.teamCategories.map(function (categoryID) {
-		categoriesPerTeam.push(
-						{id: categoryID, text: categories[categoryID].categoryName}
-		);
-	});
-
-	return categoriesPerTeam;
-}
-
-function updateTimeField(form) {
-	//todo: fix duration update without setTimeout
-	setTimeout(function () {
-		var duration = calculateDuration(
-						form.beginTimeField.timepicker('getTime'),
-						form.endTimeField.timepicker('getTime'),
-						form.pauseTimeField.timepicker('getTime'));
-
-		if (duration < 0) {
-			duration = new Date(0);
-		}
-
-		form.durationField.val(toUTCTimeString(duration));
-	}, 10);
-}
-
-function changePauseTimeField() {
-	if (this.value === '') {
-		this.value = '00:00';
-	}
-}
-
-/**
- * creates a view row with working ui components
- * @param {Object} timesheetData
- * @param {Object} entry
- * @returns {viewrow : jquery, formrow : jquery}
- */
-function renderViewRow(timesheetData, entry) {
-
-	var augmentedEntry = augmentEntry(timesheetData, entry);
-
-	var editEntryOptions = {
-		httpMethod : "put",
-		callback   : editEntryCallback,
-		ajaxUrl    : restBaseUrl + "entries/" + entry.entryID
-	};
-
-	var viewRow = prepareViewRow(timesheetData, augmentedEntry);
-	viewRow.find("button.edit").click(function () {
-		editEntryClicked(timesheetData, augmentedEntry, editEntryOptions, viewRow);
-	});
-
-	viewRow.find("button.delete").click(function () {
-		deleteEntryClicked(viewRow, entry.entryID);
-	});
-
-	return viewRow;
-}
-
-function editEntryClicked(timesheetData, augmentedEntry, editEntryOptions, viewRow) {
-	
-	var formRow = getFormRow(viewRow);
-	
-	if (formRow === undefined) {
-		formRow = renderFormRow(timesheetData, augmentedEntry, editEntryOptions);
-		viewRow.after(formRow);
-	} 
-	
-	viewRow.hide();
-	formRow.show();
-}
-
-function deleteEntryClicked(viewRow, entryID) {
-
-	var ajaxUrl = restBaseUrl + "entries/" + entryID;
-
-	var spinner = viewRow.find('span.aui-icon-wait');
-	spinner.show();
-
-	AJS.$.ajax({
-		type: 'DELETE',
-		url: ajaxUrl,
-		contentType: "application/json"
-	})
-	.then(function () {
-		var formRow = getFormRow(viewRow);
-		if(formRow !== undefined) formRow.remove();
-		viewRow.remove();
-	})
-	.fail(function (error) {
-		AJS.messages.error({
-			title: 'There was an error while deleting.',
-			body: '<p>Reason: ' + error.responseText + '</p>'
-		});
-		console.log(error);
-		spinner.hide();
+	timesheetData.entries.map(function (entry) {
+		var viewRow = prepareViewRow(timesheetData, entry);
+		timesheetTable.append(viewRow);
 	});
 }
 
@@ -609,7 +171,7 @@ function prepareViewRow(timesheetData, entry) {
   //todo: dont augment entry twice.
 	var augmentedEntry = augmentEntry(timesheetData, entry);
 
-	var viewRow = AJS.$(Confluence.Templates.Timesheet.timesheetEntry(
+	var viewRow = AJS.$(Confluence.Templates.Visualization.visualizationEntry(
 					{entry: augmentedEntry, teams: timesheetData.teams}));
 
 	viewRow.find('span.aui-icon-wait').hide();
